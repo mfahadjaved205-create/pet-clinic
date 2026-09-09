ede4c84d49596a97e50475fef0bd1b34c0077f75
mfahadjaved205-create <mfahadjaved205@gmail.com>
2026-09-09 12:29:53 +0500

    Update API endpoint to relative path

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -30,7 +30,7 @@ const SignIn = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
