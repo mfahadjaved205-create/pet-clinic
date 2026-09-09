b31ec838299a9011d28a6e1dc520ddb19e0067e4
mfahadjaved205-create <mfahadjaved205@gmail.com>
2026-09-09 12:35:51 +0500

    Update API endpoint to relative path

--- src/components/time/Time.jsx
+++ src/components/time/Time.jsx
@@ -41,7 +41,7 @@ const Time = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/bookings", {
+      const response = await fetch("/api/bookings", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
